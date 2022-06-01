037716eba8e0b83bb86eade0c6015abbbef047ac
Teddy Li <teddyxlandlee@hotmail.com>
2022-06-01 16:49:09 +0800

    <<Legacy>>, but I made it like this

---
+++ src/main/java/top/xdi8/mod/wrapper/LegacyXdi8AhoMod.java
@@ -0,0 +1,7 @@
+package top.xdi8.mod.wrapper;
+
+/** @deprecated but I made it look like this */
+@Deprecated
+@net.minecraftforge.api.distmarker.Mod("xdi8")
+public class LegacyXdi8AhoMod {
+}
---
+++ src/main/java/top/xdi8/mod/wrapper/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Wrapper package for xdi8 mod. <br />
+ *
+ * Please do not use/write anything here
+ * until we are clear about what to do here.<br />
+ *
+ * Stories should be in {@link top.xdi8.mod.firefly8 the
+ * Firefly module}.
+ */
+package top.xdi8.mod.wrapper;
