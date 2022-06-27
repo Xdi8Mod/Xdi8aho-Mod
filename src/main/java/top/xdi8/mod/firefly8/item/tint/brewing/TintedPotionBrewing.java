949d833370f2ae8de6d0a9d883c3d95c6138a22d
Teddy Li <teddyxlandlee@hotmail.com>
2022-06-27 11:39:04 +0800

    repackage

--- src/main/java/top/xdi8/mod/firefly8/item/tint/brewing/TintedPotionBrewing.java
+++ src/main/java/top/xdi8/mod/firefly8/item/tint/brewing/TintedPotionBrewing.java
@@ -1,4 +1,4 @@
-package top.xdi8.mod.firefly8.item.potion.vanilla;
+package top.xdi8.mod.firefly8.item.tint.brewing;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.Item;
@@ -8,7 +8,6 @@ import net.minecraftforge.common.brewing.IBrewingRecipe;
 import net.minecraftforge.common.brewing.VanillaBrewingRecipe;
 import net.minecraftforge.registries.RegistryObject;
 import top.xdi8.mod.firefly8.item.FireflyItems;
-import top.xdi8.mod.firefly8.item.potion.ItemTinting;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
