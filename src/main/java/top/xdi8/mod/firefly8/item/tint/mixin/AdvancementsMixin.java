5faa67ca53665196ad4545198f7fa7e0ebe3ed72
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-05 21:29:44 +0800

    fix: vanilla-advancements (test required)

--- src/main/java/top/xdi8/mod/firefly8/item/tint/mixin/AdvancementsMixin.java
+++ src/main/java/top/xdi8/mod/firefly8/item/tint/mixin/AdvancementsMixin.java
@@ -7,7 +7,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.crafting.conditions.ICondition;
 import org.apache.commons.lang3.mutable.Mutable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -16,7 +15,8 @@ import top.xdi8.mod.firefly8.item.tint.advancement.AdvancementLoadingEvent;
 @Mixin(Advancement.Builder.class)
 abstract class AdvancementsMixin {
     @Inject(at = @At("RETURN"),
-            method = "fromJson(Lcom/google/gson/JsonObject;Lnet/minecraft/advancements/critereon/DeserializationContext;Lnet/minecraftforge/common/crafting/conditions/ICondition$IContext;)Lnet/minecraft/advancements/Advancement$Builder;"
+            method = "fromJson(Lcom/google/gson/JsonObject;Lnet/minecraft/advancements/critereon/DeserializationContext;Lnet/minecraftforge/common/crafting/conditions/ICondition$IContext;)Lnet/minecraft/advancements/Advancement$Builder;",
+            remap = false   // Forge-added method
     )
     private static void hackJson(JsonObject pJson, DeserializationContext pContext, ICondition.IContext context, CallbackInfoReturnable<Advancement.Builder> cir) {
         final Advancement.Builder builder = cir.getReturnValue();
