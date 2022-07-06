cc9ebb38d836fd2a688da06fd5d997dd4edae9c1
Teddy Li <tedexe_work.top@foxmail.com>
2022-07-06 19:20:20 +0800

    Revert "todo: mobgen"
    This reverts commit 41cbd49d20cdb29d04b542eab91a6176575b18f2.

--- src/main/java/top/xdi8/mod/firefly8/Firefly8.java
+++ src/main/java/top/xdi8/mod/firefly8/Firefly8.java
@@ -10,24 +10,18 @@ import top.xdi8.mod.firefly8.entity.FireflyEntityTypes;
 import top.xdi8.mod.firefly8.item.FireflyItems;
 import top.xdi8.mod.firefly8.item.tint.brewing.TintedPotionBrewing;
 import top.xdi8.mod.firefly8.particle.FireflyParticles;
-import top.xdi8.mod.firefly8.world.FireflyMobBiomeGen;
 
 @Mod("firefly8")
 public class Firefly8 {
     public Firefly8() {
-        // Item
         FireflyItems.REGISTRY.register(modBus());
         TintedPotionBrewing.register();
 
-        // Entity
         FireflyEntityTypes.REGISTRY.register(modBus());
         modBus().addListener(this::registerEntityAttributes);
-        modBus().addListener(FireflyMobBiomeGen::onBiomeLoading);
 
-        // Particle
         FireflyParticles.REGISTRY.register(modBus());
 
-        // Client
         modBus().addListener(this::onClientSetup);
         modBus().addListener(FireflyClientSetup::registerParticles);
     }
