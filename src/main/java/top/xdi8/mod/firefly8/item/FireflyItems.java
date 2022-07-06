581d6ada2c03d3348dd20af689faab1226c7ee59
Teddy Li <tedexe_work.top@foxmail.com>
2022-07-06 19:20:20 +0800

    Revert "cleanup redundant items"
    This reverts commit 94f66e9b3041d85eb351a2bef6d71629e2c606b4.

--- src/main/java/top/xdi8/mod/firefly8/item/FireflyItems.java
+++ src/main/java/top/xdi8/mod/firefly8/item/FireflyItems.java
@@ -1,6 +1,5 @@
 package top.xdi8.mod.firefly8.item;
 
-import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -10,6 +9,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 import top.xdi8.mod.firefly8.entity.FireflyEntityTypes;
+import top.xdi8.mod.firefly8.item.technical.Xdi8AhoItem;
 
 public final class FireflyItems {
     public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, "firefly8");
@@ -17,7 +17,7 @@ public final class FireflyItems {
 
     public static final CreativeModeTab TAB = new CreativeModeTab("firefly8") {
         public @NotNull ItemStack makeIcon() {
-            return new ItemStack(XDI8AHO_ICON.get());
+            return new ItemStack(Xdi8Aho.get());
         }
     };
 
@@ -26,31 +26,22 @@ public final class FireflyItems {
             TINTED_HONEY_BOTTLE,
             TINTED_SPLASH_POTION,
             TINTED_LINGERING_POTION,
-            XDI8AHO_ICON,
-            FIREFLY_SPAWN_EGG;
+            Xdi8Aho;
 
     static {
         TINTED_GLASS_BOTTLE = REGISTRY.register("tinted_glass_bottle", () ->
-            new TintedGlassBottleItem(defaultProp()));
+            new TintedGlassBottleItem(new Item.Properties().tab(TAB)));
         TINTED_POTION = REGISTRY.register("tinted_potion", () ->
-            new TintedPotionItem(defaultProp()));
+            new TintedPotionItem(new Item.Properties().tab(TAB)));
         TINTED_HONEY_BOTTLE = REGISTRY.register("tinted_honey_bottle", () ->
-            new TintedHoneyBottleItem(defaultProp()
-                    .craftRemainder(TINTED_GLASS_BOTTLE.get())
-                    .food(Foods.HONEY_BOTTLE)
-                    .stacksTo(16)
-            ));
+            new TintedHoneyBottleItem(new Item.Properties().tab(TAB)));
         TINTED_SPLASH_POTION = REGISTRY.register("tinted_splash_potion", () ->
-            new TintedSplashPotionItem(defaultProp()));
+            new TintedSplashPotionItem(new Item.Properties().tab(TAB)));
         TINTED_LINGERING_POTION = REGISTRY.register("tinted_lingering_potion", () ->
-            new TintedLingeringPotionItem(defaultProp()));
-        XDI8AHO_ICON = REGISTRY.register("xdi8aho", () ->
-                new Item(new Item.Properties()));
-        FIREFLY_SPAWN_EGG = REGISTRY.register("firefly_spawn_egg", () ->
-                new ForgeSpawnEggItem(FireflyEntityTypes.FIREFLY,
-                        0x000000, 0x00f500,
-                        defaultProp()));
+            new TintedLingeringPotionItem(new Item.Properties().tab(TAB)));
+        Xdi8Aho = REGISTRY.register("xdi8aho", () ->
+                new Xdi8AhoItem(new Item.Properties()));
     }
-
-    static Item.Properties defaultProp() { return new Item.Properties().tab(TAB); }
+    public static final RegistryObject<ForgeSpawnEggItem> FIREFLY_SPAWN_EGG = REGISTRY.register("firefly_spawn_egg", () ->
+            new ForgeSpawnEggItem(FireflyEntityTypes.FIREFLY, 0x000000, 0x00f500, new Item.Properties().tab(TAB)));
 }
