19eac2c590f57f53ca8f8e4c1938f448ad4b3f16
Teddy Li <tedexe_work.top@foxmail.com>
2022-07-06 19:20:20 +0800

    Revert "Common bugfix: bottling (unchecked)"
    This reverts commit 948a8d215821052dc5d4dfa1d786bd1651d0d7ea.

--- src/main/java/top/xdi8/mod/firefly8/item/tint/TintedGlassBottleItem.java
+++ src/main/java/top/xdi8/mod/firefly8/item/tint/TintedGlassBottleItem.java
@@ -9,6 +9,7 @@ import net.minecraft.stats.Stats;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Bee;
@@ -37,13 +38,13 @@ public class TintedGlassBottleItem extends BottleItem {
     }
 
     @Override
-    protected @NotNull ItemStack turnBottleIntoItem(final @NotNull ItemStack pBottleStack,
-                                                    final @NotNull Player pPlayer,
-                                                    @NotNull ItemStack pFilledBottleStack) {
-        if (ItemTinting.shouldTint(pFilledBottleStack)) {
-            pFilledBottleStack = ItemTinting.tint(pFilledBottleStack);
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level pLevel, @NotNull Player pPlayer, @NotNull InteractionHand pHand) {
+        var ret = super.use(pLevel, pPlayer, pHand);
+        ItemStack stack = ret.getObject();
+        if (ItemTinting.shouldTint(stack)) {
+            return new InteractionResultHolder<>(ret.getResult(), ItemTinting.tint(stack));
         }
-        return super.turnBottleIntoItem(pBottleStack, pPlayer, pFilledBottleStack);
+        return ret;
     }
 
     /* HONEY START */
