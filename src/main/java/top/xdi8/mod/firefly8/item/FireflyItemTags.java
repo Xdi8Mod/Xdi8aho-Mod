509bf42fa98054575b8c4d0645bf9d34f147ce03
Teddy Li <tedexe_work.top@foxmail.com>
2022-07-06 19:20:20 +0800

    Revert "fix: vanilla-advancements (test required)"
    This reverts commit d93b6f805aa771dc098f334b5ed083e8d4c2029e.

--- src/main/java/top/xdi8/mod/firefly8/item/FireflyItemTags.java
+++ src/main/java/top/xdi8/mod/firefly8/item/FireflyItemTags.java
@@ -8,7 +8,8 @@ import net.minecraft.world.item.Item;
 public class FireflyItemTags {
     public static final TagKey<Item>
             TINTED_DRAGON_BREATH = create("tinted_dragon_breath"),
-            TINTED_HONEY_BOTTLES = create("tinted_honey_bottles");
+            TINTED_HONEY_BOTTLES = create("tinted_honey_bottles"),
+            BALANCED_DIET = create("balanced_diet");
 
     private static TagKey<Item> create(String path) {
         return ItemTags.create(new ResourceLocation("firefly8", path));
--- src/main/java/top/xdi8/mod/firefly8/item/tint/advancement/VanillaAdvancements.java
+++ src/main/java/top/xdi8/mod/firefly8/item/tint/advancement/VanillaAdvancements.java
@@ -6,13 +6,13 @@ import net.minecraft.advancements.critereon.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import top.xdi8.mod.firefly8.item.FireflyItemTags;
 
-import java.util.*;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = "firefly8")
@@ -24,7 +24,8 @@ public class VanillaAdvancements {
 
     private static final Map<ResourceLocation, UUID> TO_UUID = ImmutableMap.of(
             SAFELY_HARVEST_MONEY, UUID.fromString("b0ec73e8-c187-4f25-a83c-cc42263282b7"),
-            DRAGON_BREATH, UUID.fromString("fe4d3782-e9b4-431a-a4bc-5b508852ef3c"));
+            DRAGON_BREATH, UUID.fromString("fe4d3782-e9b4-431a-a4bc-5b508852ef3c"),
+            BALANCED_DIET, UUID.fromString("a1b41569-63e3-4895-89fb-e3a76d58d414"));
     private static final Map<ResourceLocation, Supplier<CriterionTriggerInstance>> TO_TRIGGER = ImmutableMap.of(
             SAFELY_HARVEST_MONEY, () -> new ItemUsedOnBlockTrigger.TriggerInstance(
                     EntityPredicate.Composite.ANY,
@@ -42,46 +43,31 @@ public class VanillaAdvancements {
                     new ItemPredicate[]{ItemPredicate.Builder.item()
                             .of(FireflyItemTags.TINTED_DRAGON_BREATH)
                             .build()}
+            ),
+            BALANCED_DIET, () -> new ConsumeItemTrigger.TriggerInstance(
+                    EntityPredicate.Composite.ANY,
+                    ItemPredicate.Builder.item()
+                            .of(FireflyItemTags.BALANCED_DIET)
+                            .build()
             )
     );
 
     @SubscribeEvent
     public static void patchTintedItem(AdvancementLoadingEvent event) {
         final ResourceLocation id = event.getId();
-        if (BALANCED_DIET.equals(id)) {
-            balancedDiet(event);
-            return;
-        }
         if (!TO_UUID.containsKey(id)) return;
-        String[][] req = event.getRequirements();
+        String[][] reqOld = event.getRequirements(),
+                   reqNew = new String[reqOld.length + 1][];
+        System.arraycopy(reqOld, 0, reqNew, 1, reqOld.length);
+
         ResourceLocation reqId = new ResourceLocation("firefly8",
                 TO_UUID.get(id).toString());
+        /*event.addCriterion(reqId, new ConsumeItemTrigger.TriggerInstance(EntityPredicate.Composite.ANY,
+                new ItemPredicate(TO_ITEM.get(id),
+                        null, MinMaxBounds.Ints.ANY, MinMaxBounds.Ints.ANY,
+                        EnchantmentPredicate.NONE, EnchantmentPredicate.NONE, null, NbtPredicate.ANY)));*/
         event.addCriterion(reqId, TO_TRIGGER.get(id).get());
-        String[] instance = req[0], newInstance = Arrays.copyOf(instance, instance.length+1);
-        newInstance[instance.length] = reqId.toString();
-        req[0] = newInstance;
-    }
-
-    /* BalancedDietHelper start */
-
-    private static void balancedDiet(AdvancementLoadingEvent event) {
-        String[][] requirements = event.getRequirements();
-
-        // 0: honey bottles
-        for (int i = 0; i < requirements.length; i++) {
-            String[] as = requirements[i];
-            if (Arrays.asList(as).contains("honey_bottle")) {
-                event.addCriterion(new ResourceLocation("firefly8:05f921ae-5f96-410d-bcce-bf20d57e5d1a"),
-                        consumeItemTrigger(FireflyItemTags.TINTED_HONEY_BOTTLES));
-                String[] nas = Arrays.copyOf(as, as.length + 1);
-                nas[as.length] = "firefly8:05f921ae-5f96-410d-bcce-bf20d57e5d1a";
-                requirements[i] = nas;
-            }
-        }
-    }
-
-    private static ConsumeItemTrigger.TriggerInstance consumeItemTrigger(TagKey<Item> tag) {
-        return new ConsumeItemTrigger.TriggerInstance(EntityPredicate.Composite.ANY,
-                ItemPredicate.Builder.item().of(tag).build());
+        reqNew[0] = new String[] {reqId.toString()};
+        event.setRequirements(reqNew);
     }
 }
