520507367e033af0b2969b3ebd4324f9442019f7
Justin Zhang <2691475984@qq.com>
2022-07-12 10:10:08 +0800

    fix portal core block

--- src/main/java/top/xdi8/mod/firefly8/block/Xdi8ahoPortalCoreBlock.java
+++ src/main/java/top/xdi8/mod/firefly8/block/Xdi8ahoPortalCoreBlock.java
@@ -1,6 +1,8 @@
 package top.xdi8.mod.firefly8.block;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
 
@@ -11,9 +13,14 @@ public class Xdi8ahoPortalCoreBlock extends Block {
                 .requiresCorrectToolForDrops()
                 .lightLevel(bs -> bs.getValue(FIREFLY_COUNT) * 3)
         );
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FIREFLY_COUNT, 0));
     }
 
     public static final IntegerProperty FIREFLY_COUNT =
             IntegerProperty.create("fireflies", 0, 5);
 
+    @Override
+    public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder){
+        pBuilder.add(FIREFLY_COUNT);
+    }
 }
