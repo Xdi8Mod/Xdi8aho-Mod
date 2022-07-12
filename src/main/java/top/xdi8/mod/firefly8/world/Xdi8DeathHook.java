4703e80cc90da9539dccf3c0fd643ec811a7701e
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-12 21:35:44 +0800

    Respawn packets

--- src/main/java/top/xdi8/mod/firefly8/world/Xdi8DeathHook.java
+++ src/main/java/top/xdi8/mod/firefly8/world/Xdi8DeathHook.java
@@ -1,10 +1,12 @@
 package top.xdi8.mod.firefly8.world;
 
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.server.players.PlayerList;
 
 public class Xdi8DeathHook {
     /** Return false if death is canceled and player is teleported. */
     public static boolean modifyIsDeathOfDying(LocalPlayer player) {
+        //player.respawn();
         return true;//TODO
     }
 }
