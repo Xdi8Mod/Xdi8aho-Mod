ac3e99c58a2d04c2e01585ccb348bcf2dd6b6b85
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-19 11:57:53 +0800

    Entity Mixin fix

--- src/main/java/org/featurehouse/mcmod/spm/mixin/ingredient/ChickenEntityMixin.java
+++ src/main/java/org/featurehouse/mcmod/spm/mixin/ingredient/ChickenEntityMixin.java
@@ -1,26 +1,21 @@
 package org.featurehouse.mcmod.spm.mixin.ingredient;
 
 import net.minecraft.world.entity.animal.Chicken;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import org.featurehouse.mcmod.spm.mixin.acc.IngredientAccessor;
+import net.minecraftforge.common.crafting.CompoundIngredient;
 import org.featurehouse.mcmod.spm.util.registries.AnimalIngredients;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
+@SuppressWarnings("unused")
 @Mixin(Chicken.class)
 abstract class ChickenEntityMixin {
     @Shadow @Final @Mutable
     private static Ingredient FOOD_ITEMS;
 
     static {
-        @SuppressWarnings("all")
-        IngredientAccessor acc = (IngredientAccessor) (Object) FOOD_ITEMS;
-        acc.setMatchingStacks(null);    // clear cache
-        //acc.cacheMatchingStacks();
-        ItemStack[] matchingStacks = FOOD_ITEMS.getItems();
-        FOOD_ITEMS = Ingredient.of(AnimalIngredients.configureChicken(matchingStacks));
+        FOOD_ITEMS = CompoundIngredient.of(FOOD_ITEMS, Ingredient.of(AnimalIngredients.chickenFood()));
     }
 }
