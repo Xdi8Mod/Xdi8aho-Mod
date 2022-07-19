c628d6d11026ca90183802115bad30daf3335d58
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-19 11:57:53 +0800

    Entity Mixin fix

--- src/main/java/org/featurehouse/mcmod/spm/util/registries/AnimalIngredients.java
+++ src/main/java/org/featurehouse/mcmod/spm/util/registries/AnimalIngredients.java
@@ -1,26 +1,18 @@
 package org.featurehouse.mcmod.spm.util.registries;
 
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import org.featurehouse.mcmod.spm.SPMMain;
 import org.featurehouse.mcmod.spm.platform.api.hook.AccParrot;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Stream;
 
 @ApiStatus.Internal
 public final class AnimalIngredients {
     private AnimalIngredients() {}
 
-    public static Stream<ItemStack> configurePig(ItemStack[] stacks) {
-        ArrayList<ItemStack> stackList = new ArrayList<>(Arrays.asList(stacks));
-        stackList.add(SPMMain.ENCHANTED_TUBER_ITEM.get().getDefaultInstance());
-        stackList.add(SPMMain.PEEL.get().getDefaultInstance());
-
-        return stackList.stream();
+    public static Item[] pigFood() {
+        return new Item[] { SPMMain.ENCHANTED_TUBER_ITEM.get(), SPMMain.PEEL.get() };
     }
 
     public static void configureParrot() {
@@ -28,10 +20,7 @@ public final class AnimalIngredients {
         parrotTamingIngredients.add(SPMMain.ENCHANTED_CROP_SEEDS.get());
     }
 
-    public static Stream<ItemStack> configureChicken(ItemStack[] stacks) {
-        ArrayList<ItemStack> stackList = new ArrayList<>(Arrays.asList(stacks));
-        stackList.add(SPMMain.ENCHANTED_CROP_SEEDS.get().getDefaultInstance());
-
-        return stackList.stream();
+    public static Item[] chickenFood() {
+        return new Item[] { SPMMain.ENCHANTED_CROP_SEEDS.get() };
     }
 }
