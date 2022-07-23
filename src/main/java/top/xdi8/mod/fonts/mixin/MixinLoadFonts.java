8942da16b91bbc793de8392c5ffb66227a07ddd3
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-23 18:59:30 +0800

    Try fix fonts (@Pseudo to avoid compile time error)

--- src/main/java/top/xdi8/mod/fonts/mixin/MixinLoadFonts.java
+++ src/main/java/top/xdi8/mod/fonts/mixin/MixinLoadFonts.java
@@ -1,6 +1,7 @@
 package top.xdi8.mod.fonts.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,6 +11,7 @@ import java.awt.*;
 import java.util.List;
 import java.util.Set;
 
+@Pseudo
 @Mixin(targets = "icyllis.modernui.forge.ModernUIForge")
 public class MixinLoadFonts {
     @Inject(at = @At("HEAD"), method = "loadFonts(Ljava/util/List;Ljava/util/Set;)V", remap = false)
