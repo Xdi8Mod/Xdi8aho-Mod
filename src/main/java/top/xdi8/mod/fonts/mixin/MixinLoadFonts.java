52b1194cb244b1bdfc27ef92227fd574372030fc
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-24 13:35:30 +0800

    Re-add mixin config: now using ASM-shelled

--- src/main/java/top/xdi8/mod/fonts/mixin/MixinLoadFonts.java
+++ src/main/java/top/xdi8/mod/fonts/mixin/MixinLoadFonts.java
@@ -2,20 +2,10 @@ package top.xdi8.mod.fonts.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import top.xdi8.mod.fonts.Xdi8FontsMod;
-
-import java.awt.*;
-import java.util.List;
-import java.util.Set;
 
 @Pseudo
-@Mixin(targets = "icyllis.modernui.forge.ModernUIForge")
+@Mixin(targets = "icyllis.modernui.forge.ModernUIForge$Client")
 public class MixinLoadFonts {
-    @Inject(at = @At("HEAD"), method = "loadFonts(Ljava/util/List;Ljava/util/Set;)V", remap = false)
-    private static void onLoadFonts(List<? extends String> configs, Set<Font> selected, CallbackInfo ci) {
-        Xdi8FontsMod.loadFont(selected);
-    }
+    // Wrapper
+    // Real things are in LoadFontsConfigWrapper
 }
