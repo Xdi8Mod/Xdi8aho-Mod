8d9eb328b1753550317c979ba6abed47fe2879ab
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-25 14:22:31 +0800

    crossplatform param name fix

--- src/main/java/top/xdi8/mod/firefly8/item/indium/IndiumPickaxeItem.java
+++ src/main/java/top/xdi8/mod/firefly8/item/indium/IndiumPickaxeItem.java
@@ -1,15 +1,12 @@
 package top.xdi8.mod.firefly8.item.indium;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
-import top.xdi8.mod.firefly8.item.FireflyItems;
 
 public class IndiumPickaxeItem extends PickaxeItem {
     public IndiumPickaxeItem(Properties pProperties) {
@@ -17,31 +14,16 @@ public class IndiumPickaxeItem extends PickaxeItem {
     }
 
     @Override
-    public boolean hurtEnemy(ItemStack pStack, @NotNull LivingEntity pTarget, @NotNull LivingEntity pAttacker) {
-        pStack.hurtAndBreak(2, pAttacker, (entity) -> entity.broadcastBreakEvent(EquipmentSlot.MAINHAND));
-        Level level = pAttacker.getLevel();
-        if (pAttacker instanceof Player){
-            if (level.isClientSide()) return true;
-            ItemStack nugget = new ItemStack(FireflyItems.INDIUM_NUGGET.get());
-            if (!((Player) pAttacker).getInventory().add(nugget)) {
-                ((Player) pAttacker).drop(nugget, true);
-            }
-        }
+    public boolean hurtEnemy(@NotNull ItemStack pStack, @NotNull LivingEntity pTarget, @NotNull LivingEntity pAttacker) {
+        if (!super.hurtEnemy(pStack, pTarget, pAttacker)) return false;
+        IndiumTier.dropNuggets(pStack, pTarget, pAttacker);
         return true;
     }
 
     @Override
-    public boolean mineBlock(@NotNull ItemStack pStack, Level pLevel, @NotNull BlockState pState, @NotNull BlockPos pPos, @NotNull LivingEntity pEntityLiving) {
-        if (!pLevel.isClientSide && pState.getDestroySpeed(pLevel, pPos) != 0.0F) {
-            pStack.hurtAndBreak(1, pEntityLiving, (entity) -> entity.broadcastBreakEvent(EquipmentSlot.MAINHAND));
-        }
-        if (pEntityLiving instanceof Player){
-            if (pLevel.isClientSide()) return true;
-            ItemStack nugget = new ItemStack(FireflyItems.INDIUM_NUGGET.get());
-            if (!((Player) pEntityLiving).getInventory().add(nugget)) {
-                ((Player) pEntityLiving).drop(nugget, true);
-            }
-        }
+    public boolean mineBlock(@NotNull ItemStack pStack, @NotNull Level pLevel, @NotNull BlockState pState, @NotNull BlockPos pPos, @NotNull LivingEntity pEntityLiving) {
+        if (!super.mineBlock(pStack, pLevel, pState, pPos, pEntityLiving)) return false;
+        IndiumTier.dropNuggets(pStack, pLevel, pState, pPos, pEntityLiving);
         return true;
     }
 }
