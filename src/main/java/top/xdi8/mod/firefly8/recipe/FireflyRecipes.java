f91d6bd675e14cf48969e134d41aca22f2961e4a
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-27 19:36:04 +0800

    Symbol Stone production recipe

---
+++ src/main/java/top/xdi8/mod/firefly8/recipe/FireflyRecipes.java
@@ -0,0 +1,28 @@
+package top.xdi8.mod.firefly8.recipe;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleItemRecipe;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class FireflyRecipes {
+    public static final DeferredRegister<RecipeType<?>> TYPES = DeferredRegister.create(new ResourceLocation("recipe_type"), "firefly8");
+    public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, "firefly8");
+
+    public static final RegistryObject<RecipeType<SymbolStoneProductionRecipe>> PRODUCE_T = ofType("produce_symbol");
+    public static final RegistryObject<RecipeSerializer<SymbolStoneProductionRecipe>> PRODUCE_S = SERIALIZERS.register("produce_symbol",
+            SymbolStoneProductionRecipe.Serializer::new);
+
+    private static <T extends Recipe<?>> RegistryObject<RecipeType<T>> ofType(String id) {
+        return TYPES.register(id, () -> new RecipeType<>() {
+            @Override
+            public String toString() {
+                return new ResourceLocation("firefly8", id).toString();
+            }
+        });
+    }
+}
