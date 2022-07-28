d9e74d4af8adba1a48f273053607d1b606add8bc
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-28 11:15:17 +0800

    x2o fire texture

--- src/main/java/top/xdi8/mod/firefly8/item/indium/IndiumChiselItem.java
+++ src/main/java/top/xdi8/mod/firefly8/item/indium/IndiumChiselItem.java
@@ -1,9 +1,17 @@
 package top.xdi8.mod.firefly8.item.indium;
 
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import org.jetbrains.annotations.NotNull;
 
 public class IndiumChiselItem extends Item {
     public IndiumChiselItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(@NotNull UseOnContext pContext) {
+        return InteractionResult.PASS;
     }
 }
