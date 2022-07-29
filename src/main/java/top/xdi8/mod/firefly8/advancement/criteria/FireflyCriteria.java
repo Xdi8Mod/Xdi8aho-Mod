512089bb4d12d52356dac0ce3c2f4cf15befba32
Teddy Li <teddyxlandlee@hotmail.com>
2022-07-29 21:08:56 +0800

    criterion trigger firefly8:die_in_xdi8aho, noArg

--- src/main/java/top/xdi8/mod/firefly8/advancement/criteria/FireflyCriteria.java
+++ src/main/java/top/xdi8/mod/firefly8/advancement/criteria/FireflyCriteria.java
@@ -4,6 +4,7 @@ import org.featurehouse.mcmod.spm.platform.api.advacement.CriterionRegistry;
 
 public class FireflyCriteria {
     public static final GetXdi8TotemTrigger GET_XDI8_TOTEM = CriterionRegistry.register(new GetXdi8TotemTrigger());
+    public static final SimpleDieInXdi8ahoTrigger DIE_IN_XDI8AHO = CriterionRegistry.register(new SimpleDieInXdi8ahoTrigger());
 
     public static void init() {}
 }
