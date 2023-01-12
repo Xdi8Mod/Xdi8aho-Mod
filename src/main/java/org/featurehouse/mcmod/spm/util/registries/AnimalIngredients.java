bbdd8de13acf034f8c367bd2f498ee7b6c03fd36
Teddy Li <teddyxlandlee@hotmail.com>
2023-01-12 11:02:31 +0800

    Fix :fabric:runClient problem Chicken will be loaded earlier than mod initializer. So when injecting ingredients, tags should be used instead of item.

--- src/main/java/org/featurehouse/mcmod/spm/util/registries/AnimalIngredients.java
+++ src/main/java/org/featurehouse/mcmod/spm/util/registries/AnimalIngredients.java
@@ -1,7 +1,9 @@
 package org.featurehouse.mcmod.spm.util.registries;
 
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import org.featurehouse.mcmod.spm.SPMMain;
+import org.featurehouse.mcmod.spm.platform.api.reg.PlatformRegister;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Set;
@@ -10,16 +12,12 @@ import java.util.Set;
 public final class AnimalIngredients {
     private AnimalIngredients() {}
 
-    public static Item[] pigFood() {
-        return new Item[] { SPMMain.ENCHANTED_TUBER_ITEM.get(), SPMMain.PEEL.get() };
-    }
-
     public static void configureParrot() {
         Set<Item> parrotTamingIngredients = net.minecraft.world.entity.animal.Parrot.TAME_FOOD;
         parrotTamingIngredients.add(SPMMain.ENCHANTED_CROP_SEEDS.get());
     }
 
-    public static Item[] chickenFood() {
-        return new Item[] { SPMMain.ENCHANTED_CROP_SEEDS.get() };
-    }
+    public static final TagKey<Item> CHICKEN_EXTRA_FOOD = PlatformRegister.spm().itemTag("chicken_extra_food");
+    //public static final TagKey<Item> PARROT_EXTRA_FOOD = PlatformRegister.spm().itemTag("parrot_extra_food");
+    public static final TagKey<Item> PIG_EXTRA_FOOD = PlatformRegister.spm().itemTag("pig_extra_food");
 }
