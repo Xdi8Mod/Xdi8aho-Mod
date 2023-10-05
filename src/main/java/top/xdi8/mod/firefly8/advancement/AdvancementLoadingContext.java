6db5b55d2552438ce22674d8536a3e528660b66f
Justin Zhang <2691475984@qq.com>
2023-10-06 00:06:08 +0800

    Fixed 3 errors and one typo: the game can initialize now, but joining a Minecraft world will still cause errors

--- src/main/java/top/xdi8/mod/firefly8/advancement/AdvancementLoadingContext.java
+++ src/main/java/top/xdi8/mod/firefly8/advancement/AdvancementLoadingContext.java
@@ -6,26 +6,33 @@ import dev.architectury.event.EventFactory;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.CriterionTriggerInstance;
 import net.minecraft.resources.ResourceLocation;
-import org.apache.commons.lang3.mutable.Mutable;
 
 import java.util.function.Consumer;
 
 @ForgeEvent
-public record AdvancementLoadingContext(ResourceLocation id,
-                                        Advancement.Builder task) {
+public class AdvancementLoadingContext {
+    // 2023-9-27: Rewrote a record class to a normal class, to avoid this exception:
+    // Type 'java/lang/Record' is not assignable to 'top/xdi8/mod/firefly8/advancement/AdvancementLoadingContext'
+    public final ResourceLocation id;
+    public final Advancement.Builder task;
+
+    public AdvancementLoadingContext(ResourceLocation id, Advancement.Builder task) {
+        this.id = id;
+        this.task = task;
+    }
 
     public static final Event<Consumer<AdvancementLoadingContext>> EVENT = EventFactory.createConsumerLoop(AdvancementLoadingContext.class);
 
     public String[][] getRequirements() {
-        return task().requirements;
+        return this.task.requirements;
     }
 
     public void setRequirements(String[][] req) {
-        task().requirements = req;
+        this.task.requirements = req;
     }
 
     public void addCriterion(ResourceLocation id,
                              CriterionTriggerInstance trigger) {
-        task().addCriterion(id.toString(), trigger);
+        this.task.addCriterion(id.toString(), trigger);
     }
 }
