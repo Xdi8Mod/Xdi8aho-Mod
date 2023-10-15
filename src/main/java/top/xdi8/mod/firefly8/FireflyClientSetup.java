5c70045a3fa56c06c82d332c527ed0d10ae4740e
Justin Zhang <2691475984@qq.com>
2023-10-15 21:56:10 +0800

    Finally fixed particle registry error

--- src/main/java/top/xdi8/mod/firefly8/FireflyClientSetup.java
+++ src/main/java/top/xdi8/mod/firefly8/FireflyClientSetup.java
@@ -1,20 +1,20 @@
 package top.xdi8.mod.firefly8;
 
-import dev.architectury.registry.client.particle.ParticleProviderRegistry;
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import dev.architectury.registry.menu.MenuRegistry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.entity.NoopRenderer;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.FoliageColor;
+import net.minecraft.world.level.block.state.BlockState;
 import org.featurehouse.mcmod.spm.platform.api.client.BlockRenderTypes;
 import top.xdi8.mod.firefly8.block.FireflyBlocks;
-import top.xdi8.mod.firefly8.client.FireflyParticle;
 import top.xdi8.mod.firefly8.client.TakeOnlyContainerScreen;
 import top.xdi8.mod.firefly8.client.Xdi8TableScreen;
 import top.xdi8.mod.firefly8.entity.FireflyEntityTypes;
-import top.xdi8.mod.firefly8.particle.FireflyParticles;
 import top.xdi8.mod.firefly8.screen.FireflyMenus;
 
 //@Mod.EventBusSubscriber(modid = "firefly8", value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -29,6 +29,10 @@ public class FireflyClientSetup implements Runnable {
             }
             return BiomeColors.getAverageFoliageColor(blockAndTintGetter, blockPos);
         }, FireflyBlocks.CEDAR_LEAVES);
+        ColorHandlerRegistry.registerItemColors((itemStack, i) -> {
+            BlockState blockState = ((BlockItem)itemStack.getItem()).getBlock().defaultBlockState();
+            return Minecraft.getInstance().getBlockColors().getColor(blockState, null, null, i);
+        }, FireflyBlocks.CEDAR_LEAVES);
         MenuRegistry.registerScreenFactory(FireflyMenus.TAKE_ONLY_CHEST.get(),
                 TakeOnlyContainerScreen::new);
         MenuRegistry.registerScreenFactory(FireflyMenus.XDI8_TABLE.get(),
@@ -37,11 +41,5 @@ public class FireflyClientSetup implements Runnable {
                 FireflyBlocks.XDI8AHO_PORTAL_TOP_BLOCK);
         BlockRenderTypes.register(RenderType.cutout(),
                 FireflyBlocks.XDI8AHO_BACK_FIRE_BLOCK);
-        ParticleProviderRegistry.register(FireflyParticles.FIREFLY.get(),
-                pSprites -> (pType, pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed) -> {
-                    var particle = new FireflyParticle(pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed);
-                    particle.pickSprite(pSprites);
-                    return particle;
-                });
     }
 }
