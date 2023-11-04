8d0f9cc4f6ee74b0b40c2a873ea0c7c8afc2b457
Justin Zhang <2691475984@qq.com>
2023-11-05 01:05:03 +0800

    Network registry fix

--- fabric/src/main/java/top/xdi8/mod/firefly8/fabric/Firefly8Fabric.java
+++ fabric/src/main/java/top/xdi8/mod/firefly8/fabric/Firefly8Fabric.java
@@ -2,11 +2,14 @@ package top.xdi8.mod.firefly8.fabric;
 
 import net.fabricmc.api.ModInitializer;
 import top.xdi8.mod.firefly8.Firefly8;
+import top.xdi8.mod.firefly8.network.FireflyNetwork;
 
 public class Firefly8Fabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Firefly8.init();
         Firefly8.commonSetup();
+        FireflyNetwork.registerClientNetwork();
+        FireflyNetwork.registerServerNetwork();
     }
 }
