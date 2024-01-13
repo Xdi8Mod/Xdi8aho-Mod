8385d29da37dd8c11eb39912da02dbeeeb847e5c
Justin Zhang <2691475984@qq.com>
2024-01-14 01:57:20 +0800

    Bugfix and add render types

--- src/main/java/top/xdi8/mod/firefly8/block/cedar/FireflyTreeFeatures.java
+++ src/main/java/top/xdi8/mod/firefly8/block/cedar/FireflyTreeFeatures.java
@@ -5,7 +5,6 @@ import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -19,27 +18,27 @@ import net.minecraft.world.level.levelgen.feature.trunkplacers.GiantTrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import top.xdi8.mod.firefly8.block.FireflyBlocks;
 import top.xdi8.mod.firefly8.util.InternalRegistryLogWrapper;
+import top.xdi8.mod.firefly8.util.SimpleStateProviderNew;
 
 public class FireflyTreeFeatures {
     public static final InternalRegistryLogWrapper LOG_WRAPPER = InternalRegistryLogWrapper.firefly8("tree_features");
-    public static Block CEDAR_LOG = FireflyBlocks.CEDAR_LOG.get();
-    public static Block CEDAR_LEAVES = FireflyBlocks.CEDAR_LEAVES.get();
+    public static SimpleStateProviderNew CEDAR_LOG = new SimpleStateProviderNew(FireflyBlocks.CEDAR_LOG);
+    public static SimpleStateProviderNew CEDAR_LEAVES = new SimpleStateProviderNew(FireflyBlocks.CEDAR_LEAVES);
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> CEDAR = FeatureUtils.register("cedar",
-            Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(CEDAR_LOG),
-                    new StraightTrunkPlacer(7, 3, 2), BlockStateProvider.simple(CEDAR_LEAVES),
+            Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(CEDAR_LOG,
+                    new StraightTrunkPlacer(7, 3, 2), CEDAR_LEAVES,
                     new SpruceFoliagePlacer(UniformInt.of(2, 3), UniformInt.of(0, 2), UniformInt.of(1, 2)),
                     new TwoLayersFeatureSize(2, 0, 2)).ignoreVines().build());
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> MEGA_CEDAR = FeatureUtils.register("mega_cedar",
-            Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(CEDAR_LOG),
-                    new GiantTrunkPlacer(15, 2, 16),
-                    BlockStateProvider.simple(CEDAR_LEAVES),
+            Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(CEDAR_LOG,
+                    new GiantTrunkPlacer(15, 2, 16), CEDAR_LEAVES,
                     new MegaPineFoliagePlacer(ConstantInt.of(0), ConstantInt.of(0), UniformInt.of(13, 17)),
                     new TwoLayersFeatureSize(1, 1, 2))
                     .decorators(ImmutableList.of(new AlterGroundDecorator(BlockStateProvider.simple(Blocks.PODZOL)))).build());
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> MEGA_CEDAR_PINE = FeatureUtils.register("mega_cedar_pine",
-            Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(CEDAR_LOG),
-                    new GiantTrunkPlacer(15, 2, 16),
-                    BlockStateProvider.simple(CEDAR_LEAVES),
+            Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(CEDAR_LOG,
+                    new GiantTrunkPlacer(15, 2, 16), CEDAR_LEAVES,
                     new MegaPineFoliagePlacer(ConstantInt.of(0), ConstantInt.of(0), UniformInt.of(3, 7)),
                     new TwoLayersFeatureSize(1, 1, 2))
-                    .decorators(ImmutableList.of(new AlterGroundDecorator(BlockStateProvider.simple(Blocks.PODZOL)))).build());}
+                    .decorators(ImmutableList.of(new AlterGroundDecorator(BlockStateProvider.simple(Blocks.PODZOL)))).build());
+}
